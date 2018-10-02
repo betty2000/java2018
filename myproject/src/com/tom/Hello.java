808f091c30cb7cacfe96a76f047d3e70ca9c4c98
user <user@es302n15>
2018-10-02 11:08:32 +0800

    second

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -4,6 +4,18 @@ public class Hello {
 
 	public static void main(String[] args) {
 	System.out.println( "Hello world");
-			}
+	int age = 18;
+	float weight = 52.3f;
+	float height = 1.63f;
+	System.out.println( "age");
+	System.out.println( "weight");
+	System.out.println( "height");
+	
+	
+	
+			
+			
+			
+	}
 
 }
