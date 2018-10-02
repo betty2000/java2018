f6051004bee79d7df7a702d1af90f67f1a154db8
betty <user@es302n15>
2018-10-02 11:08:10 +0800

    second

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -3,7 +3,14 @@ package com.fju.Hello;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello") 
+		int age = 18;
+		float weight = 52.5f;
+		float height =1.63f;
+		System.out.println("age")
+		System.out.println();
+		
+		
 	}
 
 }
