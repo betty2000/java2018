effe2ec2eeed5f1eb06cf33855f4dc17029cdfe3
user <user@es302n19>
2018-10-09 12:01:50 +0800

    second

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -1,21 +1,49 @@
 package com.tom;
 
-public class Hello {
-
+public class Hello{
+	
 	public static void main(String[] args) {
-	System.out.println( "Hello world");
+	Personn p = new Person();
+	p.weight = 52.5f;
+	p.height = 1.63f;
+	System.out.println(p.bmi());
+	p.hello();
+	person hank = new Person();
+//	Person jack=null;
+//	jack.hello();
+	/*System.out.println( "Hello world");
 	int age = 18;
 	float weight = 52.3f;
 	float height = 1.63f;
 	System.out.println( "age");
 	System.out.println( "weight");
-	System.out.println( "height");
-	
-	
-	
-			
-			
+	System.out.println( "height");*/
+
+		
+		
 			
 	}
+
+	public static void main(String[] args) {
+		Personn p = new Person();
+		p.weight = 52.5f;
+		p.height = 1.63f;
+		System.out.println(p.bmi());
+		p.hello();
+		person hank = new Person();
+	//	Person jack=null;
+	//	jack.hello();
+		/*System.out.println( "Hello world");
+		int age = 18;
+		float weight = 52.3f;
+		float height = 1.63f;
+		System.out.println( "age");
+		System.out.println( "weight");
+		System.out.println( "height");*/
+	
+			
+			
+				
+		}
 
 }
